1846b472d05d5c4b4614701a5fce3fe89bc2bb0b
Fernando Oliva <fernando.oliva.hueto@gmail.com>
2014-06-25 11:13:06 +0200

    First Commit

---
+++ ipShoot/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using ipShoot.Shoots;
+using System.Text.RegularExpressions;
+
+namespace ipShoot
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShoot_Click(object sender, RoutedEventArgs e)
+        {
+            txbMachineName.Text = Shoot.GetMachineName();
+
+            foreach (var ip in Shoot.GetLocalIPs())
+            {
+                txbIPNames.Text = txbIPNames.Text + ip.AddressFamily + ": " + ip.ToString() + Environment.NewLine;
+            } 
+        }
+
+        private void btnRemoteShoot_Click(object sender, RoutedEventArgs e)
+        {
+            if (!txtHostName.Text.Equals(string.Empty))
+            {
+                txbIpRemote.Text = string.Empty;
+
+                foreach (var ip in Shoot.GetRemoteIP(txtHostName.Text))
+                {
+                    txbIpRemote.Text = txbIpRemote.Text + ip.AddressFamily + ": " + ip.ToString() + Environment.NewLine;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Type a host name!");
+            }
+        }
+    }
+}
---
+++ ipShoot/Shoots/Shoot.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace ipShoot.Shoots
+{
+    public static class Shoot
+    {
+        /// <summary>
+        /// Get the local machine name
+        /// </summary>        
+        /// <returns>string machineName</returns>
+        public static string GetMachineName()
+        {
+            return Dns.GetHostName();
+        }
+
+        /// <summary>
+        /// Get local IPs
+        /// </summary>
+        /// <returns></returns>
+        public static List<IPAddress> GetLocalIPs()
+        {
+            System.Text.ASCIIEncoding ASCII = new System.Text.ASCIIEncoding();
+
+            List<IPAddress> ips = new List<IPAddress>();
+
+            IPHostEntry heserver = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in heserver.AddressList)
+            {
+                ips.Add(ip);
+            }
+
+            return ips;
+        }
+
+        /// <summary>
+        /// Get IP of specified host
+        /// </summary>
+        /// <param name="hostName"></param>
+        /// <returns></returns>
+        public static List<IPAddress> GetRemoteIP(string hostName)
+        {
+            try
+            {
+                IPAddress[] ipaddress = Dns.GetHostAddresses(hostName);
+
+                return ipaddress.ToList();
+            }
+            catch (Exception ex)
+            {
+                List<IPAddress> ipaddress = new List<IPAddress>();
+
+                return ipaddress;
+            }            
+        }
+    }
+}
